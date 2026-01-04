d63432b6d5eb21dc5f4534f65de88664b2455e22
GABRIEL-SORIN SABO <gabriel.sorin.sabo@stud.ubbcluj.ro>
2026-01-04 18:14:57 +0200

    Laborator 9 cu tot cu buton delete final pentru lista

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -20,4 +20,48 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        // Asigura-te c? shopl.ID nu este 0 (vezi sectiunea de mai jos)
+        if (shopl.ID != 0)
+        {
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
+    }
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        var selectedProduct = listView.SelectedItem as Product;
+
+        if (selectedProduct != null)
+        {
+            var shopl = (ShopList)BindingContext;
+
+            await App.Database.DeleteListProductAsync(
+                shopl.ID,
+                selectedProduct.ID
+            );
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+            listView.SelectedItem = null;
+        }
+        else
+        {
+            await DisplayAlert(
+                "Selection Required",
+                "Please select a product from the list to delete it.",
+                "OK"
+            );
+        }
+    }
+
 }
