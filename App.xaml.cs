aaacf53a799aa8ec1cf62e0d0bac2c0f12c549ed
GABRIEL-SORIN SABO <gabriel.sorin.sabo@stud.ubbcluj.ro>
2026-01-04 16:42:09 +0200

    Laborator 8 cu MAUI functional (v finala)

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,21 @@
-﻿namespace SaboGabrielLab7
+﻿using SaboGabrielLab7.Data;
+namespace SaboGabrielLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
